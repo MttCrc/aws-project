e53b5a209f1edd3c200ac69a2efa1574a24a93ff
MttCrc <mattiacroce04@gmail.com>
2023-04-11 11:50:10 +0200

    First commit

---
+++ db_connection.php
@@ -0,0 +1,17 @@
+<?php
+$ip = "localhost";
+$db_name = "croce_progetto_cloud_computing";
+$password = "";
+$username = "root";
+$connection = new mysqli($ip, $username, $password, $db_name);
+$response = [];
+
+if ($connection->connect_error) {
+  $response['message'] = 'Connection failed';
+  http_response_code(400);
+  echo json_encode($response);
+  die;
+}
+
+return $connection;
+?>
