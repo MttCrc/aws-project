8546f87fb88775ad7c0e6acb70ee5c152e1d26e8
MttCrc <mattiacroce04@gmail.com>
2023-04-11 12:30:41 +0200

    home + roba

--- login.php
+++ login.php
@@ -1,31 +1,21 @@
 <?php
-include 'db_connection.php';
-$response = [];
-session_start();
-
-$nickname = mysqli_real_escape_string($connection, $_POST["nickname"]);
-$pass = mysqli_real_escape_string($connection, $_POST["password"]);
-
-$sql = "SELECT id, nickname FROM users
-WHERE nickname = ? AND password = SHA2(CONCAT(?, (SELECT salt FROM users WHERE nickname = ?)), 256);";
-
-$statement = $connection->prepare($sql);
-
-$statement->bind_param("sss", $nickname, $pass, $nickname);
-$query_response = [];
-$statement->bind_result($query_response['id'], $query_response['nickname']);
-$statement->execute();
-
-if ($statement->fetch()){
-  $_SESSION['user_id'] = $query_response['id'];
-  $_SESSION['nickname'] = $query_response['nickname'];
-  $response['message'] = 'login riusciuto';
-  
-}else{
-  http_response_code(400);
-  $response['message'] = 'login fallito';
-}
-$statement->close();
-
-echo json_encode($response);
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+
+    <body style="text-align: center">
+        <h1>Login</h1>
+
+        <form action="getlog.php" method="post">
+            <label for="nickname">nickname</label><br>
+            <input type="text" name="nickname" maxlength="100" required/><br>
+            <label for="password">password</label><br>
+            <input type="password" name="password" maxlength="100" required/><br>
+            <input type="submit"/><br>
+        </form>
+        <br><br>
+    </body>
+</html>
